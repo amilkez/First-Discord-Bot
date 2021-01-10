3bc3be58b67b970e7088a2cc0308fbd6bb06930c
Angel <amurphynous@gmail.com>
2021-01-10 15:32:38 -0600

    First Bot Test

---
+++ bot.js
@@ -0,0 +1,37 @@
+require('dotenv').config()
+
+
+const Discord = require('discord.js');
+const client = new Discord.Client({
+    partials: ["MESSAGE"]
+});
+
+const BOT_PREFIX = '!'
+const MOD_ME_COMMAND = 'MOD0ME'
+
+client.on('ready', () => {
+    console.log('Our bot is ready to go')
+})
+
+client.on('messageDelete', msg => {
+    msg.channel.send(`Stop deleting messages!`)
+})
+
+client.on('message', msg => {
+    if (msg.content != '') {
+        msg.react('❤️')
+    }
+
+    if (msg.content === `${BOT_PREFIX}${MOD_ME_COMMAND}`) {
+        modUser(msg.member)
+    }
+    if(msg.content === "!mod-me") {
+        msg.member.roles.add("797937514262036531")
+    }
+})
+
+function modUser(member) {
+    member.roles.add('797937514262036531')
+}
+
+client.login(process.env.BOT_TOKEN)
